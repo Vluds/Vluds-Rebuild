ae5c4af4f258d274b24ff31bc693c7f85668363b
TymmeSyde <tymmesyde@gmail.com>
2015-03-23 22:25:41 +0100

    Add JQuery animation to the homepage

--- src/js/JAnimate.js
+++ src/js/JAnimate.js
@@ -1,10 +1,18 @@
+$(document).ready(function()
+{
+	$('header').stop().fadeIn(1000).queue(function(){
+		$('section').stop().fadeIn(1000);
+		$('.account').stop().animate({marginBottom: "0"}, 400).dequeue();
+	});
+});
+
 $(document).on('mouseenter', '.button', function()
 {
-	$(this).stop().animate({backgroundColor: "transparent"});
-	$('p', this).stop().animate({color: "rgba(37, 199, 125, 1)"});
+	$(this).stop().animate({backgroundColor: "rgba(37, 199, 125, 1)"}, 400);
+	$('p', this).stop().animate({color: "rgba(255, 255, 255, 1)"}, 200);
 
 }).on('mouseleave', '.button', function() 
 {
-	$(this).stop().animate({backgroundColor: "rgba(37, 199, 125, 1)"});
-	$('p', this).stop().animate({color: "rgba(255, 255, 255, 1)"});
+	$(this).stop().animate({backgroundColor: "transparent"}, 400);
+	$('p', this).stop().animate({color: "rgba(37, 199, 125, 1)"}, 200);
 });
