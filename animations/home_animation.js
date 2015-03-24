8c87d28bdcceef28a249ae520d42eacd9fb72c47
TymmeSyde <tymmesyde@gmail.com>
2015-03-24 17:02:33 +0100

    Update animations

--- animations/home_animation.js
+++ animations/home_animation.js
@@ -1,5 +1,6 @@
 $('#home-container').ready(function()
 {
 	$('#home-container').fadeIn(400).css("display", "inline-block");
+	$('#text-container').stop().animate({marginTop: "0"}, 400);
 	$('.account').stop().animate({opacity: 1, bottom: "0"}, 400);
 });
