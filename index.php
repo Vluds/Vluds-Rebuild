6430c5af183f33fe07d96aec194e0a35b1863ab5
TymmeSyde <tymmesyde@gmail.com>
2015-05-31 23:38:26 +0200

    Update: meta data, php directory

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 	session_start();
 
+	// GET URL SITE
+	$siteProtocol = strpos(strtolower($_SERVER['SERVER_PROTOCOL']),"https") === FALSE ? "http" : "https";
+	$siteHost = $_SERVER['HTTP_HOST'];
+	$siteUrl = $siteProtocol."://".$siteHost;
+	define('URL', $siteUrl, true);
+
+	// Define directory.
+	define('ROOT', str_replace('index.php', '', $_SERVER['SCRIPT_FILENAME']), true);
+	define('WEBROOT', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']), true);
+
+	define('IMG', WEBROOT.'img/', true);
+
+	// Define social media url.
+	define('TWITTER', 'https://www.twitter.com/vluds_', true);
+	define('FACEBOOK', 'https://www.facebook.com/vluds', true);
+
+	// Initialization.
 	require("src/php/class/bdd.php");
 	$newStaticBdd = new BDD();
 
@@ -12,12 +29,44 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
-		<title>Vluds - Be more social.</title>
-		<link rel="stylesheet" type="text/css" href="css/default_style.css">
+		<meta name="language" content="french">
+		<meta name="google" content="notranslate">
 
+		<title>Vluds - Be more social.</title>
+		<meta name="author" content="vluds.eu">
+		<meta name="description" content="Réseau social pour les personnes créatives. Venez découvrir des créations, rencontrez des personnes, discutez avec elles et partagez !" />
+		<meta name="keywords" content="vluds, réseau, social, créatives, création, partarger, découvrir, tags, ..." />
+
+		<link rel="stylesheet" type="text/css" href="css/default_style.css">
 		<link rel="icon" href="img/favicon.ico" />
 
 		<base href="" >
+
+
+		<meta name="apple-mobile-web-app-title" content="Vluds - Be more social.">
+
+		<!-- Open Graph data -->
+		<meta property="og:locale" content="fr_FR" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Vluds - Be more social." />
+		<meta property="og:description" content="Réseau social pour les personnes créatives. Venez découvrir des créations, rencontrez des personnes, discutez avec elles et partagez !" />
+		<meta property="og:url" content="<?php echo URL;?>" />
+		<meta property="og:site_name" content="vluds" />
+		<meta property="og:image" content="<?php echo URL.IMG;?>preview.jpg" />
+
+		<!-- Facebook spec -->
+		<meta property="article:publisher" content="<?php echo FACEBOOK;?>" />
+
+		<!-- Twitter data -->
+		<meta property="twitter:card" content="summary_large_image" />
+		<meta property="twitter:site" content="@vluds_" />
+		<meta property="twitter:domain" content="vluds" />
+		<meta property="twitter:creator" content="@vluds_" />
+		<meta property="twitter:image" content="<?php echo URL.IMG;?>preview.jpg" />
+		<meta property="twitter:title" content="Vluds - Be more social." />
+		<meta property="twitter:description" content="Réseau social pour les personnes créatives. Venez découvrir des créations, rencontrez des personnes, discutez avec elles et partagez !" />
+		<meta property="twitter:url" content="<?php echo TWITTER;?>" />
+
 
 		<!-- Script -->
 		<script src="src/js/lib/jquery.min-1.11.1.js"></script>
