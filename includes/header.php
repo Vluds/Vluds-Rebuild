28745086a9f6d3871c73838076262f1be932608b
TymmeSyde <tymmesyde@gmail.com>
2015-06-10 20:35:17 +0200

    Update: sql database

--- includes/header.php
+++ includes/header.php
@@ -3,13 +3,13 @@
 		<li id="logo" onClick="loadModel('home')"><h1>vluds</h1></li>
 	</ul>
 
-	<ul id="right-container">
+	<ul id="right-container" onClick="navBarAction()">
 <?php
 	if(User::isLogged())
 	{
 ?>
 		<li id="username"><h3><?php echo User::getUsername();?></h3></li>
-		<li id="navbar-button" onClick="navBarAction()"></li>
+		<li id="navbar-button"</li>
 <?php
 	}
 ?>
--- vluds_rebuild_bdd.sql
+++ vluds_rebuild_bdd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 27 Mai 2015 à 21:13
+-- Généré le :  Mer 10 Juin 2015 à 20:33
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -39,11 +39,37 @@ CREATE TABLE IF NOT EXISTS `color_ref` (
 
 INSERT INTO `color_ref` (`id`, `name`, `ref`) VALUES
 (0, 'green', 'rgb(75, 214, 150)'),
-(1, 'red', 'rgb(213, 76, 76)'),
-(2, 'blue', 'rgb(64, 111, 180)'),
+(1, 'red', 'rgb(255, 64, 64)'),
+(2, 'blue', 'rgb(52, 161, 211)'),
 (3, 'orange', 'rgb(238, 125, 56)'),
 (4, 'purple', 'rgb(125, 72, 214)'),
-(5, 'yelow', 'rgb(214, 209, 72)');
+(5, 'yelow', 'rgb(222, 215, 31)');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `links`
+--
+
+CREATE TABLE IF NOT EXISTS `links` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id_linker` int(11) NOT NULL,
+  `user_id_linked` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tags`
+--
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
 
 -- --------------------------------------------------------
 
@@ -54,6 +80,7 @@ INSERT INTO `color_ref` (`id`, `name`, `ref`) VALUES
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `activated` int(11) NOT NULL,
+  `completed` int(11) NOT NULL,
   `activationKey` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `avatar` varchar(255) NOT NULL,
@@ -66,7 +93,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `token` varchar(255) NOT NULL,
   `time` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_tags`
+--
+
+CREATE TABLE IF NOT EXISTS `user_tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
